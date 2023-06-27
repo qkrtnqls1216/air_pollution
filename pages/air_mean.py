9c40c86341d7211242f7b5111cd3b6b38de084fd
qkrtnqls1216 <79692357+qkrtnqls1216@users.noreply.github.com>
2023-06-27 10:37:28 +0900

    air

--- pages/air_mean.py
+++ pages/air_mean.py
@@ -11,10 +11,41 @@ from datetime import datetime
 df['Measurement date'] = pd.to_datetime(df['Measurement date'])
 df['hour'] = df['Measurement date'].dt.hour
 
-# Check if any columns contain non-numeric values
-non_numeric_cols = []
-for col in df.columns:
-    if not np.issubdtype(df[col].dtype, np.number):
-        non_numeric_cols.append(col)
+numeric_cols = ['SO2', 'NO2', 'O3', 'CO', 'PM10', 'PM2.5']
+df_numeric = df[numeric_cols]
 
-st.write("Non-numeric columns:", non_numeric_cols)
+# Check for non-numeric values
+non_numeric_values = df_numeric.apply(pd.to_numeric, errors='coerce').isna().sum()
+non_numeric_cols = non_numeric_values[non_numeric_values > 0].index.tolist()
+
+if non_numeric_cols:
+    st.write("The following columns contain non-numeric values:")
+    st.write(non_numeric_cols)
+    st.stop()
+
+data = df_numeric.groupby(df['hour'], as_index=False).mean()
+
+# Create subplots
+fig = go.Figure()
+
+# Add traces for SO2, NO2, and O3
+fig.add_trace(go.Scatter(x=data['hour'], y=data['SO2'], mode='lines', name='SO2'))
+fig.add_trace(go.Scatter(x=data['hour'], y=data['NO2'], mode='lines', name='NO2'))
+fig.add_trace(go.Scatter(x=data['hour'], y=data['O3'], mode='lines', name='O3'))
+
+# Create subplot for PM10 and PM2.5
+fig.add_trace(go.Scatter(x=data['hour'], y=data['PM10'], mode='lines', name='PM10', yaxis='y2'))
+fig.add_trace(go.Scatter(x=data['hour'], y=data['PM2.5'], mode='lines', name='PM2.5', yaxis='y2'))
+
+# Set layout
+fig.update_layout(
+    title='Air Pollution Levels by Hour',
+    xaxis=dict(title='Hour'),
+    yaxis=dict(title='SO2, NO2, O3', type='linear'),
+    yaxis2=dict(title='PM10, PM2.5', type='log', overlaying='y', side='right'),
+    legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1),
+    template='plotly_white'
+)
+
+# Use Plotly to render the figure in Streamlit
+st.plotly_chart(fig)
