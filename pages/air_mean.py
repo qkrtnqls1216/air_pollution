394ff1f47b2857b6cab81957f002e547fd35195b
qkrtnqls1216 <79692357+qkrtnqls1216@users.noreply.github.com>
2023-06-27 10:44:27 +0900

    air

--- pages/air_mean.py
+++ pages/air_mean.py
@@ -1,14 +1,18 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 
 # 전체 데이터 읽어들이기
 df = pd.read_csv("https://media.githubusercontent.com/media/qkrtnqls1216/air_pollution/main/Measurement_summary.csv", encoding='cp949')
 
-# 열별로 데이터 유형 확인하기
-non_numeric_cols = []
-for col in df.columns:
-    if not np.issubdtype(df[col].dtype, np.number):
-        non_numeric_cols.append(col)
+# 숫자 열의 이름
+numeric_cols = ['SO2', 'NO2', 'O3', 'CO', 'PM10', 'PM2.5']
 
-st.write("Non-numeric columns:", non_numeric_cols)
+# 비숫자 값을 NaN으로 변환
+df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='coerce')
+
+# 숫자 열의 평균 계산
+mean_values = df[numeric_cols].mean()
+
+# 평균 출력
+for col, mean_value in mean_values.items():
+    st.write(f"Mean of {col}: {mean_value}")
