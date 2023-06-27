db1497c923922d95a624693aa62da50cf9a23119
qkrtnqls1216 <79692357+qkrtnqls1216@users.noreply.github.com>
2023-06-27 11:19:56 +0900

    air

--- pages/air_mean.py
+++ pages/air_mean.py
@@ -13,12 +13,14 @@ df = pd.read_csv(
     "https://media.githubusercontent.com/media/qkrtnqls1216/air_pollution/main/Measurement_summary.csv",
     encoding='cp949'
 )
+location = df.groupby('Station code')['PM2.5'].agg([np.mean]) # MAP에 PM2.5를 표현하기 위해 (PM2.5의 평균)
+location['Latitude'] = df['Latitude'].unique() # df의 Latitude 열 반환하여 각 지역의 PM2.5평균에 맞게 저장 
+location['Longitude'] = df['Longitude'].unique()  # df의 Longitude 열 반환
 
 from datetime import datetime
 
 df['Measurement date'] = df['Measurement date'].astype('datetime64')
 df['hour'] = df.loc[:, "Measurement date"].dt.hour
-# df = df.drop('Measurement date', axis=1)
 
 data = df.groupby('hour', as_index=False).agg({'SO2':'mean', 'NO2':'mean', 'O3':'mean', 'CO':'mean', 'PM10':'mean', 'PM2.5':'mean'})
 
