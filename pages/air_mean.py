e2ccc3d2deb58780ae676d8e9ba9fe28688462f0
qkrtnqls1216 <79692357+qkrtnqls1216@users.noreply.github.com>
2023-06-27 10:52:19 +0900

    air

--- pages/air_mean.py
+++ pages/air_mean.py
@@ -1,31 +1,28 @@
-import os
-import numpy as np
-import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
-%matplotlib inline
-
-# 전체 데이터 읽어들이기
-df = pd.read_csv(
-    "https://media.githubusercontent.com/media/qkrtnqls1216/air_pollution/main/Measurement_summary.csv",
-    encoding='cp949'
-)
-df
-
-from datetime import datetime
-
-df['Measurement date'] = df['Measurement date'].astype('datetime64')
-df['hour'] = df.loc[:, "Measurement date"].dt.hour
-# df = df.drop('Measurement date', axis=1)
-
-data = df.groupby('hour', as_index=False).agg({'SO2':'mean', 'NO2':'mean', 'O3':'mean', 'CO':'mean', 'PM10':'mean', 'PM2.5':'mean'})
-data
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
 
 # 전체평균
-plt.figure(figsize=(10,10))
-air_1 = data.plot(x='hour', y=['SO2','NO2','O3'])
-air_1.grid()
-air_2 = data.plot(x='hour', y=['PM10', 'PM2.5'])
-air_2.grid()
-plt.yscale("log")
+fig = go.Figure()
+
+# Line plots for 'SO2', 'NO2', and 'O3'
+fig.add_trace(go.Scatter(x=data['hour'], y=data['SO2'], name='SO2'))
+fig.add_trace(go.Scatter(x=data['hour'], y=data['NO2'], name='NO2'))
+fig.add_trace(go.Scatter(x=data['hour'], y=data['O3'], name='O3'))
+
+fig.update_layout(title='Average Air Pollution Levels (Hourly)',
+                  xaxis_title='Hour',
+                  yaxis_title='Concentration')
+
+st.plotly_chart(fig)
+
+# Line plots for 'PM10' and 'PM2.5'
+fig2 = go.Figure()
+fig2.add_trace(go.Scatter(x=data['hour'], y=data['PM10'], name='PM10'))
+fig2.add_trace(go.Scatter(x=data['hour'], y=data['PM2.5'], name='PM2.5'))
+
+fig2.update_layout(title='Average PM10 and PM2.5 Levels (Hourly)',
+                   xaxis_title='Hour',
+                   yaxis_title='Concentration')
+
+st.plotly_chart(fig2)
