dd65cfa15d797e27a2abc789f046e386b986f566
qkrtnqls1216 <79692357+qkrtnqls1216@users.noreply.github.com>
2023-06-27 14:16:40 +0900

    air

--- pages/pollution.py
+++ pages/pollution.py
@@ -12,8 +12,7 @@ import common
 # 전체 데이터 읽어들이기
 common.page_config()
 
-st.title("""2017-03-03 Dongjak-gu 
-Pollution Level""")
+st.title("""2017-03-03 Dongjak-gu Pollution Level""")
 
 df = common.get_sales()
 df['Measurement date'] = df['Measurement date'].astype('str')
