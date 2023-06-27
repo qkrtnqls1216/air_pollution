3ae5c50414df9620f29ec8442202004f60ae8163
qkrtnqls1216 <79692357+qkrtnqls1216@users.noreply.github.com>
2023-06-27 11:30:31 +0900

    air

--- pages/air_mean.py
+++ pages/air_mean.py
@@ -10,11 +10,12 @@ import folium
 
 # 전체 데이터 읽어들이기
 df = pd.read_csv(
-    "https://media.githubusercontent.com/media/qkrtnqls1216/air_pollution/main/Measurement_summary.csv"
+    "https://media.githubusercontent.com/media/qkrtnqls1216/air_pollution/main/Measurement_summary.csv",
+    encoding='cp949'
 )
-location = df.groupby('Station code')['PM2.5'].agg([np.mean]) # MAP에 PM2.5를 표현하기 위해 (PM2.5의 평균)
-location['Latitude'] = df['Latitude'].unique() # df의 Latitude 열 반환하여 각 지역의 PM2.5평균에 맞게 저장 
-location['Longitude'] = df['Longitude'].unique()  # df의 Longitude 열 반환
+data[['Latitude', 'Longitude']] = data[['Latitude', 'Longitude']].replace("-", np.NaN)
+data[['Latitude', 'Longitude']] = data[['Latitude', 'Longitude']].astype('f')
+data.dropna(inplace=True)
 
 from datetime import datetime
 
