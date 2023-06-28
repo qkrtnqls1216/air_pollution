b2b5a73f4465c91b46a4bad26ecdc8b27ecc1a19
qkrtnqls1216 <79692357+qkrtnqls1216@users.noreply.github.com>
2023-06-28 09:15:06 +0900

    air

--- pages/Data.py
+++ pages/Data.py
@@ -1,8 +1,8 @@
+# Data.py (데이터 불러온 곳)
 import streamlit as st
 import common
 
 common.page_config()
 st.title("Data")
-st.dataframe(common.get_sales(),
-             use_container_width=True,
-             hide_index=True)
+st.divider()
+st.dataframe(common.get_sales(), use_container_width=True, hide_index=True)
