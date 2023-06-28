721c32ea3ba957a4a87b7e5e889611951761ad0a
qkrtnqls1216 <79692357+qkrtnqls1216@users.noreply.github.com>
2023-06-28 09:40:22 +0900

    air

--- pages/04_Barplot.py
+++ pages/04_Barplot.py
@@ -12,21 +12,28 @@ st.title("SO2 barplot")
 st.divider()
 df = common.get_sales()
 
-# SO2 비율이 높은 정보 10개 출력
-SO2 = df.sort_values(by = ['SO2'], ascending=False)
+empty1,con1 = st.colums([0.5,0.3])
 
-SO2_Address = df.groupby('Address').agg({'SO2' : 'median'}).sort_values('SO2',ascending=False).reset_index()
-# Address를 기준으로 그룹화하여 SO2 집단별 평균으로 내림차순으로 정렬
-# reset_index -> 인덱스 리셋(단순한 정수 인덱스로 세팅)
+def main():
+    with empyt1:
+        empty()
+    
+    with con1:
+        # SO2 비율이 높은 정보 10개 출력
+        SO2 = df.sort_values(by = ['SO2'], ascending=False)
 
-# 상위 10개 데이터만 저장
-SO2 = SO2_Address.sort_values('SO2',ascending=False).head(10)
+        SO2_Address = df.groupby('Address').agg({'SO2' : 'median'}).sort_values('SO2',ascending=False).reset_index()
+        # Address를 기준으로 그룹화하여 SO2 집단별 평균으로 내림차순으로 정렬
+        # reset_index -> 인덱스 리셋(단순한 정수 인덱스로 세팅)
+
+        # 상위 10개 데이터만 저장
+        SO2 = SO2_Address.sort_values('SO2',ascending=False).head(10)
 
 
-plt.figure(figsize=(12,35))
+        plt.figure(figsize=(12,35))
 
-plt.subplot(6,1,1)
-sns.barplot(y="Address", x="SO2", data = SO2_Address.head(10))
+        plt.subplot(6,1,1)
+        sns.barplot(y="Address", x="SO2", data = SO2_Address.head(10))
 
-# st_folium()
-st.pyplot(plt)
+        # st_folium()
+        st.pyplot(plt)
